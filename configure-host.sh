602a1a363bbf975742fbb22ab074ca14f2bd51ba
shaun422 <nauhszuralliv@gmail.com>
2026-08-05 19:48:11 -0400

    almost done final tests

--- configure-host.sh
+++ configure-host.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-
-# -------------------------------------------------------
 # configure-host.sh
 # COMP2137 Assignment 3
 # Shaun Leonel VIllaruz - 200635694
-##!/bin/bash
-# ------------------------------------------------------
 
 #This ignors Ctrl+C, terminate, and hangup signlas
 trap '' INT TERM HUP
@@ -15,7 +11,7 @@ VERBOSE=false
 
 #Variables that store values display of the commands
 NEW_HOSTNAME=""
-NEW_IP""
+NEW_IP=""
 ENTRY_NAME=""
 ENTRY_IP=""
 
@@ -97,6 +93,77 @@ if [ -n "$NEW_HOSTNAME" ]; then
     else
 
         show_msg "Hostname is already correct."
+
+    fi
+
+fi
+
+#This if statement will configure the IP address if -ip was used
+if [ -n "$NEW_IP" ]; then
+
+    #Gets the current IP address from the LAN interface
+    CURRENT_IP=$(ip -4 addr show eth1 | grep inet | awk '{print $2}' | cut -d/ -f1)
+
+    #Checks if the IP address needs to be changed
+    if [ "$CURRENT_IP" != "$NEW_IP" ]; then
+
+        show_msg "Changing IP address..."
+        #Updates the IP address inside the Netplan file
+        sed -i "s/$CURRENT_IP\/24/$NEW_IP\/24/g" /etc/netplan/10-lxc.yaml
+
+        #Updates the current machine's IP inside /etc/hosts
+        sed -i "s/$CURRENT_IP/$NEW_IP/g" /etc/hosts
+
+        #Applies the new network settings
+        netplan apply
+
+        #Records the change in the system log
+        logger "IP address has changed from $CURRENT_IP to $NEW_IP"
+
+        show_msg "IP address updated successfully."
+    else
+
+        show_msg "IP address is already correct."
+
+    fi
+
+fi
+
+#This if statement will add or update a host entry if -hostentry was used
+if [ -n "$ENTRY_NAME" ] && [ -n "$ENTRY_IP" ]; then
+
+    #Checks if the hostname already exists inside /etc/hosts
+    if grep -q "[[:space:]]$ENTRY_NAME$" /etc/hosts; then
+
+        #Gets the current IP connected to the hostname
+        CURRENT_ENTRY_IP=$(grep "[[:space:]]$ENTRY_NAME$" /etc/hosts | awk '{print $1}')
+
+        #Checks if the host entry has the correct IP address
+        if [ "$CURRENT_ENTRY_IP" != "$ENTRY_IP" ]; then
+
+            #Updates the old host entry with the new IP address
+            sed -i "s/^$CURRENT_ENTRY_IP.*$ENTRY_NAME$/$ENTRY_IP $ENTRY_NAME/" /etc/hosts
+
+            #Records the change in the system log
+            logger "Host entry $ENTRY_NAME has changed from $CURRENT_ENTRY_IP to $ENTRY_IP"
+
+            show_msg "Host entry for $ENTRY_NAME was updated."
+
+        else
+
+            show_msg "Host entry for $ENTRY_NAME is already correct."
+
+        fi
+
+    else
+
+        #Adds the new hostname and IP address to /etc/hosts
+        echo "$ENTRY_IP $ENTRY_NAME" >> /etc/hosts
+
+        #Records the new host entry in the system log
+        logger "Host entry $ENTRY_NAME with IP $ENTRY_IP was added"
+
+        show_msg "Host entry for $ENTRY_NAME was added."
 
     fi
 
